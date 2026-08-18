eafe7183883f83f5c30576fc02da7b96bf9b574e
nroj95 <236590761+nroj95@users.noreply.github.com>
2026-08-18 19:25:59 +0200

    remove obsolete favorites sort shortcuts

--- static/build/App/romscripts/favorites/favsort.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-scriptlabel="Sort favorites (A-Z)"
-scriptinfo="Sorts the favorites\nfile alphabetically."
-
-tools favsort
--- static/build/App/romscripts/favorites/favsort2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-scriptlabel="Sort favorites (by system)"
-scriptinfo="Sorts the favorites\nfile by game system."
-
-tools favsort2
